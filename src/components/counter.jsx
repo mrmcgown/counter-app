28eedae797621314a16ff3d003c6e5a074576bd9
mrmcgown <mrmcgown@gmail.com>
2018-10-01 16:19:25 -0400

    add code through Components 10.Handling Events

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,10 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component } from "react"
 
 class Counter extends Component {
-    state = {  }
-    render() { 
-        return (  );
-    }
+  state = {
+    count: 0
+  }
+
+  handleIncrement() {
+    console.log("Increment Clicked")
+  }
+
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+      </div>
+    )
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-"
+    classes += this.state.count === 0 ? "warning" : "primary"
+    return classes
+  }
+
+  formatCount() {
+    const { count } = this.state
+    return count === 0 ? "Zero" : count
+  }
 }
- 
-export default Counter;
+
+export default Counter
