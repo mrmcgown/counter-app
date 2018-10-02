6aa3926e5de18d06f80598018788833bad90f1b0
mrmcgown <mrmcgown@gmail.com>
2018-10-02 12:13:56 -0400

    Finish counter-app

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,8 +5,9 @@ class Counter extends Component {
     count: 0
   }
 
-  handleIncrement() {
-    console.log("Increment Clicked")
+  handleIncrement = product => {
+    console.log(product)
+    this.setState({ count: this.state.count + 1 })
   }
 
   render() {
@@ -14,7 +15,7 @@ class Counter extends Component {
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={product => this.handleIncrement(product)}
           className="btn btn-secondary btn-sm"
         >
           Increment
